d6a8ec161d61644cc0ad9146e37b401938e2bf01
fada21 <lukasz.czaplicki@gmail.com>
2014-11-15 13:56:12 +0000

    logging parse cmd sending

--- android/wear/src/main/java/caprica6/fblandroidhackathon/org/caprica6/CommandDispatcher.java
+++ android/wear/src/main/java/caprica6/fblandroidhackathon/org/caprica6/CommandDispatcher.java
@@ -1,13 +1,35 @@
 package caprica6.fblandroidhackathon.org.caprica6;
 
+import android.util.Log;
+
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class CommandDispatcher {
 
     public static void dispatch(Command command) {
         ParseObject po = new ParseObject(Command.commandParseObject);
         po.add("cmd", command.command);
-        po.saveInBackground();
+        po.saveInBackground(new LoggingSaveCallback(command));
+    }
+
+    public static class LoggingSaveCallback extends SaveCallback {
+
+        private Command cmd;
+
+        LoggingSaveCallback(Command command) {
+            cmd = command;
+        }
+
+        @Override
+        public void done(ParseException e) {
+            if (e == null) {
+                Log.d("ParseWearDrone", String.format("Command %s sent", cmd.command));
+            } else {
+                Log.d("ParseWearDrone", String.format("Command not sent due to error", e.getMessage()));
+            }
+        }
     }
 
 }
